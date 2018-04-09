85c4879aa858dc6e33be2c44348115aa7120343f
niuhongtao <18333602833@163.com>
2018-04-09 17:28:51 +0800

    test-04/09-commit

---
+++ 页面切换/change.js
@@ -0,0 +1,45 @@
+/**
+ * Created by 18333 on 2018/2/8.
+ */
+/*Javascript代码片段*/
+var animateApp = angular.module('animateApp', ['ngRoute', 'ngAnimate']);
+
+animateApp.config(['$routeProvider',
+    function($routeProvider) {
+        $routeProvider.
+        when('/home', {
+            templateUrl: 'home.htm',
+            controller: 'HomeController'
+        }).
+        when('/about', {
+            templateUrl: 'about.htm',
+            controller: 'AboutController'
+        }).
+        when('/contact', {
+            templateUrl: 'contact.htm',
+            controller: 'ContactController'
+        }).
+        otherwise({
+            redirectTo: '/home'
+        });
+    }]);
+
+
+// CONTROLLERS ============================================
+// home page controller
+animateApp.controller('HomeController', function($scope) {
+    $scope.pageClass = 'page-home';
+    $scope.message = "<这里是首页>";
+});
+
+// about page controller
+animateApp.controller('AboutController', function($scope) {
+    $scope.pageClass = 'page-about';
+    $scope.message = "<这里是关于页>";
+});
+
+// contact page controller
+animateApp.controller('ContactController', function($scope) {
+    $scope.pageClass = 'page-contact';
+    $scope.message = "<这里是联系页>";
+});
